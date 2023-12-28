baa04542e5f3d0e278c10574311aa05ad7f36307
gabriel234239094 <130863137+Gabcdc@users.noreply.github.com>
2023-12-28 08:48:55 -0300

    TREINO

---
+++ aula012/ex011.js
@@ -0,0 +1,10 @@
+var idade = 70
+
+if(idade < 14 ){
+    console.log('Não vota ')
+}else if(idade <= 18 || idade > 65 ){
+    console.log('Voto opcional')
+}else{
+    
+    console.log('Voto obrigatorio ')
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'eua '
+console.log(`Você é do ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Então você é Brasileiro')
+
+}
+else{
+    console.log('Então você é Estranjeiro')
+}
