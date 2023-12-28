66c1a2dc0905f4e6550b401a549214fccab508f0
gabriel234239094 <130863137+Gabcdc@users.noreply.github.com>
2023-12-28 19:36:40 -0300

    rf

---
+++ aula14ex/ex016/escript.js
@@ -0,0 +1,9 @@
+function contar(){
+    let ini = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+
+    if(ini.value.length == 0){
+        window.alert('[ERRO]')
+    }
+}
