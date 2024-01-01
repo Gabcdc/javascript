91ab12f804acc25473ed19b2b7725c83f76a2575
gabriel234239094 <130863137+Gabcdc@users.noreply.github.com>
2023-12-31 21:49:24 -0300

    cda

--- aula14ex/ex016/escript.js
+++ aula14ex/ex016/escript.js
@@ -2,8 +2,30 @@ function contar(){
     let ini = window.document.getElementById('txti')
     let fim = window.document.getElementById('txtf')
     let passo = window.document.getElementById('txtp')
+    let res  = window.document.getElementById('res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!!')
+    }else{
+        res.innerHTML = 'Contando:'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if(p <=0){
+            window.alert('Passo invalido! Considerando passo 1 ')
+            p = 1 
+        }
 
-    if(ini.value.length == 0){
-        window.alert('[ERRO]')
+        if(i < f){
+
+            for(var c  = i; c <= f; c += p){
+            res.innerHTML +=`  ${c} 👉`
+        }
+        
+    }else{
+        for(let c = i; c >=f; c -=p){
+            res.innerHTML += `${c} 👉`
+        }
+    }
+    res.innerHTML += "🏳️‍🌈"
     }
 }
