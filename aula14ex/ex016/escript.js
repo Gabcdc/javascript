e97c76e36b362c79fb1f5921d84e8e440428f435
gabriel234239094 <130863137+Gabcdc@users.noreply.github.com>
2024-01-02 11:18:30 -0300

    fxxh

--- aula14ex/ex016/escript.js
+++ aula14ex/ex016/escript.js
@@ -1,31 +1,52 @@
 function contar(){
     let ini = window.document.getElementById('txti')
     let fim = window.document.getElementById('txtf')
-    let passo = window.document.getElementById('txtp')
-    let res  = window.document.getElementById('res')
+    let passo  = window.document.getElementById('txtp')
+    let res = window.document.getElementById('res')
+
+
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados!!')
-    }else{
-        res.innerHTML = 'Contando:'
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-        if(p <=0){
-            window.alert('Passo invalido! Considerando passo 1 ')
-            p = 1 
-        }
-
-        if(i < f){
-
-            for(var c  = i; c <= f; c += p){
-            res.innerHTML +=`  ${c} 👉`
+        res.innerHTML = 'Impossivel Contar! '
+        window.alert('digite novamente!')
+        
+    }
+    else{
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        if (p <= 0){
+            window.alert('Impossivel considerando passoo 1')
+            p = 1
         }
         
-    }else{
-        for(let c = i; c >=f; c -=p){
-            res.innerHTML += `${c} 👉`
+        
+        
+        
+        if(i < f){
+           
+        for(let C = i;C <= f; C += p) {
+
+            res.innerHTML += `${C } \u{1F449}`
+            
         }
+        
+    } 
+    else {
+        if(i < f){
+           for(C = i;C >= f;C-=p){
+        res.innerHTML += `${C } \u{1F449}`
+   
     }
-    res.innerHTML += "🏳️‍🌈"
-    }
+    
+
+} 
+        }
+    
+
+res.innerHTML += `\u{1F3C1}`
+}
+
+
 }
